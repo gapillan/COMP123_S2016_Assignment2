7f07f41c727bce899effd188d7387434fc57495c
gapillan <gpaillan@my.centennialcollege.ca>
2016-06-10 14:10:12 -0700

    Added a Hero and SuperHero Class

--- COMP123_S2016_Assignment2/Program.cs
+++ COMP123_S2016_Assignment2/Program.cs
@@ -6,10 +6,22 @@ using System.Threading.Tasks;
 
 namespace COMP123_S2016_Assignment2
 {
+    /**
+     * This class is the driver class for our Program
+     * 
+     * @class Program
+     */
     class Program
     {
+         /**
+         * The main method for our driver class Program
+         *
+         * @method Main
+         * @param {string[]} args
+         */
         static void Main(string[] args)
         {
+          
         }
     }
 }
