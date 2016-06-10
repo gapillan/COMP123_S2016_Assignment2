6a35d3187f4fe4d09aed7f355fbb1263209b56f1
gapillan <gpaillan@my.centennialcollege.ca>
2016-06-10 15:19:29 -0700

    Added private and public variables

--- COMP123_S2016_Assignment2/Hero.cs
+++ COMP123_S2016_Assignment2/Hero.cs
@@ -8,5 +8,31 @@ namespace COMP123_S2016_Assignment2
 {
     class Hero
     {
+        // PRIVATE INSTANCE VARIABLES ++++++++
+        private int _strength;
+        private int _speed;
+        private int _health;
+        private string _name;
+
+        // PUBLIC PROPERTIES +++++++++++++++++
+
+        /**
+         * <summary>
+         * This is a property for our private _name field
+         * </summary>
+         * 
+         * @property {string} Name
+         */
+        public string Name
+        {
+            get 
+            { 
+                return this._name; 
+            }
+            set 
+            { 
+                this._name = value; 
+            }
+        }
     }
 }
